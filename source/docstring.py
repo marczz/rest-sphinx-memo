2af8bff3da1a382ae201ca370420aa793c947740
Marc Zonzon <marc.zonzon+git@gmail.com>
2012-01-31 21:17:20 +0100

    source/docstring.py: new file, example of docstring

---
+++ source/docstring.py
@@ -0,0 +1,26 @@
+def example(self, arg1, arg2):
+    """Docstring example.
+
+    :Parameters:
+      - `arg1` (float) - the first value
+      - `arg2` (float) - the second value
+
+    :Returns:
+        arg1/arg2
+
+    :Returns Type:
+        float
+
+    :Examples:
+
+    >>> import template
+    >>> a = MainClass()
+    >>> a.example(6,2)
+    1.5
+
+    .. note:: can be useful to emphasize
+       important features.
+    .. seealso:: :py:exc:`ZeroDivisionError`
+    .. warning:: arg2 must be non-zero.
+    .. todo:: check that arg2 is non zero.
+"""
