917b4ed642e8063b8dc16ad058969f1b2bad75da
Marc Zonzon <marc.zonzon+git@gmail.com>
2017-05-26 16:59:57 +0200

    divide.py: example of docstring, pure Sphinx syntax.

---
+++ source/code/divide.py
@@ -0,0 +1,14 @@
+def divide(self, i, j):
+    """Divide two numbers.
+
+    This function is to show how the docstring look, using pure Sphinx syntax,
+    with explicit fields.
+
+    :param i: numerator
+    :type i: int
+    :param j: denominator
+    :type j: int
+    :return: quotient
+    :rtype: integer
+    :raises: :exc:`ZeroDivisionError`
+"""
