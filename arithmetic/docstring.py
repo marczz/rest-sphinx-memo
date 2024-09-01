d8f826ce38a928318a5031d7412772e07b9cf089
Marc Zonzon <marc.zonzon+git@gmail.com>
2024-09-01 09:29:34 +0200

    docstring.py: switched to an example in Numpy syntax.

--- arithmetic/docstring.py
+++ arithmetic/docstring.py
@@ -1,26 +1,50 @@
 def example(arg1, arg2):
-    """Docstring example.
+    """ Divide two float numbers
 
-    :Parameters:
-      - `arg1` (float) - the first value
-      - `arg2` (float) - the second value
+    This is a Numpy docstring example.
 
-    :Returns:
-        arg1/arg2
+    Parameters
+    ----------
+    arg1 : float
+        the first value
+    arg2 : float
+        the second value
 
-    :Returns Type:
-        float
+    Returns
+    -------
+    float
+        arg1 divided by arg2
 
-    :Examples:
+    Raises
+    ------
+    ZeroDivisionError
+       when the second value is null.
 
+    Examples
+    --------
     >>> import template
     >>> a = MainClass()
     >>> a.example(6,2)
     1.5
 
-    .. note:: can be useful to emphasize
-       important features.
-    .. seealso:: :py:exc:`ZeroDivisionError`
-    .. warning:: arg2 must be non-zero.
-    .. todo:: check that arg2 is non zero.
-"""
+    Warning
+    -------
+    arg2 must be non-zero.
+
+    Notes
+    -----
+    Make sure we are using numpy syntax [1]_
+
+    See Also
+    --------
+    :py:exc:`ZeroDivisionError`
+
+    Todo
+    ----
+    We should use type inference
+
+    References
+    ----------
+    .. [1] `Numpy Style Guide`_
+
+    """
